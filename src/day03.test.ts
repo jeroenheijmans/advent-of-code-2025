1cd1fe85289943e42f3957152e88ccc72df0c406
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-04 00:31:13 +0100

    Day 03 refactor (reuse solution)

--- src/day03.test.ts
+++ src/day03.test.ts
@@ -15,28 +15,10 @@ function parseInput(input: string) {
     .map((x) => x);
 }
 
-function part1(data: string[]) {
+function solveFor(data: string[], substringSize = 2) {
     let answer = 0;
     data.forEach(line => {
-      let pos1 = 0;
-      for (let i = 1; i < line.length - 1; i++) {
-        if (line[i]! > line[pos1]!) pos1 = i;
-      }
-      let pos2 = line.length - 1;
-      for (let i = pos2 - 1; i > pos1; i--) {
-        if (line[i]! > line[pos2]!) pos2 = i;
-      }
-      const combo = line[pos1]! + line[pos2]!;
-      const joltage = parseInt(combo);
-      answer += joltage;
-    })
-    return answer;
-}
-
-function part2(data: string[]) {
-    let answer = 0;
-    data.forEach(line => {
-      let digits = 12;
+      let digits = substringSize;
       let combo = "";
       let lastPos = -1;
 
@@ -59,22 +41,22 @@ describe(`${day}`, async () => {
   const input = await Bun.file(`src/${day}.txt`).text();
 
   it("should solve part 1 (example 1)", () => {
-    const result = part1(parseInput(example1));
+    const result = solveFor(parseInput(example1));
     expect(result).toEqual(357);
   });
 
   it("should solve part 1", () => {
-    const result = part1(parseInput(input));
+    const result = solveFor(parseInput(input));
     expect(result).toEqual(17311);
   });
 
   it("should solve part 2 (example 1)", () => {
-    const result = part2(parseInput(example1));
+    const result = solveFor(parseInput(example1), 12);
     expect(result).toEqual(3121910778619);
   });
 
   it("should solve part 2", () => {
-    const result = part2(parseInput(input));
+    const result = solveFor(parseInput(input), 12);
     expect(result).toEqual(171419245422055);
   });
 }); 

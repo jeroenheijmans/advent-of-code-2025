717624e167ed9f07140dc6690514872d0f4e4ee4
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-04 20:29:23 +0100

    Day 04 part 1

--- src/day04.test.ts
+++ src/day04.test.ts
@@ -2,18 +2,47 @@ import { describe, expect, it } from "bun:test";
 
 const day = "day04";
 
-const example1 = ``;
+const example1 = `
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`;
+
+type Point = {key: string, x: number, y: number, c: string};
 
 function parseInput(input: string) {
   return input
     .split(/\r?\n/gi)
     .filter((x) => !!x)
-    .map((x) => x);
+    .map((line, y) => line.split("").map((c, x) => ({ key: `${x};${y}`, x, y, c})))
+    .flat();
 }
 
-function part1(data: any) {
-    let answer = 0;
-    return answer;
+function getNeighbors(p: Point, data: { [key: string]: Point }) {
+  const result = [];
+  for (let dy = -1; dy < 2; dy++) {
+    for (let dx = -1; dx < 2; dx++) {
+      if (dy == 0 && dx == 0) continue;
+      const key = `${p.x + dx};${p.y + dy}`;
+      if (data[key]) result.push(data[key]);
+    }
+  }
+  return result;
+}
+
+function part1(data: Point[]) {
+    const map = data.reduce((prev, curr) => ({ ...prev, [curr.key]: curr }), {});
+    return data
+      .filter(p => p.c === "@")
+      .map(p => getNeighbors(p, map).filter(n => n.c === "@").length)
+      .filter(len => len < 4)
+      .length;
 }
 
 function part2(data: any) {
@@ -26,12 +55,12 @@ describe(`${day}`, async () => {
 
   it("should solve part 1 (example 1)", () => {
     const result = part1(parseInput(example1));
-    expect(result).toEqual(-1);
+    expect(result).toEqual(13);
   });
 
   it("should solve part 1", () => {
     const result = part1(parseInput(input));
-    expect(result).toEqual(-1);
+    expect(result).toEqual(1384);
   });
 
   // it("should solve part 2 (example 1)", () => {

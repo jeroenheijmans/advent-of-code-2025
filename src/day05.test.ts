8324be55e3f01a111168eab0bba8468133fe6eaf
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-05 11:58:47 +0100

    Day 05 part 2 (incorrect)

--- src/day05.test.ts
+++ src/day05.test.ts
@@ -18,32 +18,72 @@ const example1 = `
 function parseInput(input: string) {
   const [ranges, ids] = input.split(/\r?\n\r?\n/gi);
   return {
-    ranges: ranges!.split(/\r?\n/gi).filter((x) => !!x).map((x) => x.split("-").map(n => parseInt(n))),
-    ids: ids!.split(/\r?\n/gi).filter((x) => !!x).map(id => parseInt(id)),
+    ranges: ranges!
+      .split(/\r?\n/gi)
+      .filter((x) => !!x)
+      .map((x) => x.split("-").map((n) => parseInt(n))),
+    ids: ids!
+      .split(/\r?\n/gi)
+      .filter((x) => !!x)
+      .map((id) => parseInt(id)),
   };
 }
 
 type Input = {
   ranges: number[][];
-  ids: number[]
-} 
+  ids: number[];
+};
 
 function isFresh(id: number, ranges: number[][]) {
-  return ranges.some(range => {
+  return ranges.some((range) => {
     const [from, to] = range;
     if (id >= from! && id <= to!) return true;
   });
 }
 
 function part1(data: Input) {
-    return data.ids
-      .filter(id => isFresh(id, data.ranges))
-      .length;
+  return data.ids.filter((id) => isFresh(id, data.ranges)).length;
+}
+
+function isOverlapping(r1: number[], r2: number[]) {
+  const [from1, to1] = r1;
+  const [from2, to2] = r2;
+  if (from2! >= from1! && from2! <= to1!) return true;
+  if (to2! >= from1! && to2! <= to1!) return true;
+  return false;
+}
+
+function mergeRanges(r1: number[], r2: number[]) {
+  const [from1, to1] = r1;
+  const [from2, to2] = r2;
+  return [
+    Math.min(from1!, to1!, from2!, to2!),
+    Math.max(from1!, to1!, from2!, to2!),
+  ]
 }
 
 function part2(data: Input) {
-    let answer = 0;
-    return answer;
+  let { ranges } = data;
+  while (true) {
+    let hasDoneMerge = false;
+    for (let i = 0; i < ranges.length; i++) {
+      for (let j = i + 1; j < ranges.length; j++) {
+        if (isOverlapping(ranges[i]!, ranges[j]!)) {
+          hasDoneMerge = true;
+          const merged = mergeRanges(ranges[i]!, ranges[j]!);
+          // console.log("Merging!", ranges[i], ranges[j], "becomes", merged);
+          ranges = [
+            ...ranges.filter(r => r !== ranges[i] && r !== ranges[j]),
+            merged
+          ]
+          break;
+        }
+      }
+      if (hasDoneMerge) break;
+    }
+    if (!hasDoneMerge) break;
+  }
+  return ranges.map(([from, to]) => to! - from! + 1).reduce((a, b) => a + b, 0);
 }
 
 describe(`${day}`, async () => {
@@ -56,16 +96,17 @@ describe(`${day}`, async () => {
 
   it("should solve part 1", () => {
     const result = part1(parseInput(input));
-    expect(result).toEqual(-1);
+    expect(result).toEqual(520);
   });
 
-  // it("should solve part 2 (example 1)", () => {
-  //   const result = part2(parseInput(example1));
-  //   expect(result).toEqual(-1);
-  // });
+  it("should solve part 2 (example 1)", () => {
+    const result = part2(parseInput(example1));
+    expect(result).toEqual(14);
+  });
 
-  // it("should solve part 2", () => {
-  //   const result = part2(parseInput(input));
-  //   expect(result).toEqual(-1);
-  // });
-}); 
+  it("should solve part 2", () => {
+    const result = part2(parseInput(input));
+    expect(result).toBeLessThan(352347720583355);
+    expect(result).toEqual(-1);
+  });
+});

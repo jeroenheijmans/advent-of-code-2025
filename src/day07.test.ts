1a4c31bc244a19f1887a67d960b14551acd14dc0
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-06 11:56:59 +0100

    Day 07 prep

---
+++ src/day07.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "bun:test";
+
+const day = "day07";
+
+const example1 = ``;
+
+function parseInput(input: string) {
+  return input
+    .split(/\r?\n/gi)
+    .filter((x) => !!x)
+    .map((x) => x);
+}
+
+function part1(data: any) {
+  let answer = 0;
+  return answer;
+}
+
+function part2(data: any) {
+  let answer = 0;
+  return answer;
+}
+
+describe(`${day}`, async () => {
+  const input = await Bun.file(`src/${day}.txt`).text();
+
+  it("should solve part 1 (example 1)", () => {
+    const result = part1(parseInput(example1));
+    expect(result).toEqual(-1);
+  });
+
+  // it("should solve part 1", () => {
+  //   const result = part1(parseInput(input));
+  //   expect(result).toEqual(-1);
+  // });
+
+  // it("should solve part 2 (example 1)", () => {
+  //   const result = part2(parseInput(example1));
+  //   expect(result).toEqual(-1);
+  // });
+
+  // it("should solve part 2", () => {
+  //   const result = part2(parseInput(input));
+  //   expect(result).toEqual(-1);
+  // });
+});

93e76aa3618c5e57017e9c444d8f3a5dcf78054b
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-07 12:45:53 +0100

    Day 07 part 2

--- src/day07.test.ts
+++ src/day07.test.ts
@@ -56,7 +56,31 @@ function part1(data: string[][]) {
 
 function part2(data: string[][]) {
   let answer = 0;
-  return answer;
+
+  let lines = data
+    .map((line, y) => line.map((c, x) => ({ x, y, c })));
+
+  const worlds = lines.map(line => line.map((entry) => entry.c === "S" ? 1 : 0));
+  worlds.push(worlds[0]!.map(() => 0));
+  let y = 0;
+
+  do {
+    for (let x = 0; x < lines[0]!.length; x++) {
+      const paths = worlds[y][x];
+      if (paths === 0) continue;
+
+      if (lines[y] && lines[y]![x]?.c === "^") {
+        worlds[y + 1][x - 1] += paths;
+        worlds[y + 1][x + 1] += paths;
+        answer++;
+      } else {
+        worlds[y + 1][x] += paths;
+      }
+    }
+  } while (++y < lines.length);
+
+
+  return worlds.pop()?.reduce((a, b) => a + b, 0);
 }
 
 describe(`${day}`, async () => {
@@ -72,13 +96,13 @@ describe(`${day}`, async () => {
     expect(result).toEqual(1490);
   });
 
-  // it("should solve part 2 (example 1)", () => {
-  //   const result = part2(parseInput(example1));
-  //   expect(result).toEqual(-1);
-  // });
+  it("should solve part 2 (example 1)", () => {
+    const result = part2(parseInput(example1));
+    expect(result).toEqual(40);
+  });
 
-  // it("should solve part 2", () => {
-  //   const result = part2(parseInput(input));
-  //   expect(result).toEqual(-1);
-  // });
+  it("should solve part 2", () => {
+    const result = part2(parseInput(input));
+    expect(result).toEqual(3806264447357);
+  });
 });

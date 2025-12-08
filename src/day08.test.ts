43f3695fae0e68e7128b34ee52ed2bd27fb963a3
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-08 21:22:15 +0100

    Day 08 part 1 (incorrect)

--- src/day08.test.ts
+++ src/day08.test.ts
@@ -2,18 +2,79 @@ import { describe, expect, it } from "bun:test";
 
 const day = "day08";
 
-const example1 = ``;
+const example1 = `
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689`;
 
 function parseInput(input: string) {
   return input
     .split(/\r?\n/gi)
     .filter((x) => !!x)
-    .map((x) => x);
+    .toSorted()
+    .map((x) => x.split(",").map(n => parseInt(n)))
+    .map(coords => ({
+      key: `${coords[0]!};${coords[1]!};${coords[2]!}`,
+      coords
+    } as Point));
 }
 
-function part1(data: any) {
-  let answer = 0;
-  return answer;
+type Point = {
+  key: string;
+  coords: [number, number, number];
+};
+
+const distance = (a: Point, b: Point): number => 
+  Math.sqrt(
+    Math.pow(Math.abs(a.coords[0] - b.coords[0]), 2)
+    + Math.pow(Math.abs(a.coords[1] - b.coords[1]), 2)
+    + Math.pow(Math.abs(a.coords[2] - b.coords[2]), 2)
+  );
+
+function part1(data: Point[], maxPairs = 1000) {
+  const distances = data
+    .map((p1, i) => data
+      .filter((p2, j) => i < j &&  p1 !== p2)
+      .map(p2 => ({
+        p1,
+        p2,
+        estimate: distance(p1, p2)
+      })))
+    .flat()
+    .toSorted((a, b) => a.estimate - b.estimate);
+
+  const map = data.reduce((acc, next) => {
+    acc[next.key] = new Set<string>([next.key]);
+    return acc;
+  }, { } as Record<string, Set<string>>)
+
+  for (let i = 0; i < maxPairs; i++) {
+    const {p1, p2} = distances[i]!;
+    const union = new Set([...map[p1.key]!, ...map[p2.key]!]);
+    for (const key of union) {
+      map[key] = union;
+    }
+  }
+
+  const circuits = [...new Set(Object.values(map))].map(x => x.size).toSorted()
+  return circuits.pop()! * circuits.pop()! * circuits.pop()!;
 }
 
 function part2(data: any) {
@@ -25,14 +86,15 @@ describe(`${day}`, async () => {
   const input = await Bun.file(`src/${day}.txt`).text();
 
   it("should solve part 1 (example 1)", () => {
-    const result = part1(parseInput(example1));
-    expect(result).toEqual(-1);
+    const result = part1(parseInput(example1), 10);
+    expect(result).toEqual(40);
   });
 
-  // it("should solve part 1", () => {
-  //   const result = part1(parseInput(input));
-  //   expect(result).toEqual(-1);
-  // });
+  it("should solve part 1", () => {
+    const result = part1(parseInput(input));
+    expect(result).toBeGreaterThan(729);
+    expect(result).toEqual(-1);
+  });
 
   // it("should solve part 2 (example 1)", () => {
   //   const result = part2(parseInput(example1));

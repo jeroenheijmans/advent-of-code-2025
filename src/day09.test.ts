c98543a212c1b9d598f1cfe5b3b2bc9f47fbe053
Jeroen Heijmans <github@jeroenheijmans.nl>
2025-12-16 21:42:45 +0100

    Day 09 part 2 (new direction)

--- src/day09.test.ts
+++ src/day09.test.ts
@@ -38,59 +38,82 @@ function part1(data: Point[]) {
 function part2(data: Point[]) {
   let answer = 0;
 
-  const sides = data.map((p1, i) => {
-    const p2 = data[i === data.length - 1 ? 0 : i + 1];
-    return {
-      x1: Math.min(p1!.x, p2!.x),
-      x2: Math.max(p1!.x, p2!.x),
-      y1: Math.min(p1!.y, p2!.y),
-      y2: Math.max(p1!.y, p2!.y),
-      isHorizontal: p1!.y === p2!.y,
-      isVertical: p1!.x === p2!.x,
-    };
-  });
+  const sides = [];
+
+  // it's a wild guess that this will always work
+  let outside = "top";
+  let previousSide = {
+    x1: data[0]!.x - 0.5,
+    x2: data[0]!.x - 0.5,
+    y1: data[0]!.y - 0.5,
+    y2: data[0]!.y - 0.5,
+  };
 
   for (let i = 0; i < data.length; i++) {
-    for (let j = i + 1; j < data.length; j++) {
-      const width = Math.abs(data[i]!.x - data[j]!.x) + 1;
-      const height = Math.abs(data[i]!.y - data[j]!.y) + 1;
-      const area = width * height;
+    const p1 = data[i]!;
+    const p2 = data[(i + 1) % data.length]!;
+    const p3 = data[(i + 2) % data.length]!;
+    const side = {
+      x1: previousSide.x2,
+      x2: p2.x,
+      y1: previousSide.y2,
+      y2: p2.y,
+    };
 
-      const fromx = Math.min(data[i]!.x, data[j]!.x);
-      const fromy = Math.min(data[i]!.y, data[j]!.y);
-      const tox = Math.max(data[i]!.x, data[j]!.x);
-      const toy = Math.max(data[i]!.y, data[j]!.y);
-
-      let isCorrectlyEnclosed = true;
-      for (let x = fromx; x <= tox; x++) {
-        // Check if the vertical line fits in any box
-        const relevantSides = sides.filter(s => s.isHorizontal && x >= s.x1 && x <= s.x2);
-
-        if (relevantSides.some(s => s.y1 < toy && s.y1 > fromy)) {
-          if (data[j]!.x === 2 && data[j]!.y === 3)
-            console.log(area.toFixed(0).padStart(3, " "), "Disqualifying at", x, "cause", `${data[i]!.x},${data[i]!.y}`, "to", `${data[j]!.x},${data[j]!.y}`, 'because', relevantSides.filter(s => s.y1 < toy && s.y1 > fromy));
-          isCorrectlyEnclosed = false;
-          break;
-        }
-
-        // for (let y = fromy + 1; y <= toy; y++) {
-        //   const side = relevantSides.find(s => s.y1 === y);
-        //   if (side && y < toy) {
-        //     console.log(area, "Disqualifying", `${data[i]!.x},${data[i]!.y}`, "to", `${data[j]!.x},${data[j]!.y}`, 'while at', x, y);
-        //     // console.log("Disqualifying", `${data[i]!.x},${data[i]!.y}`, "to", `${data[j]!.x},${data[j]!.y}`, 'while at', x, y, 'based on', side);
-        //     isCorrectlyEnclosed = false;
-        //     break;
-        //   }
-        // }
-        // if (!isCorrectlyEnclosed) break;
+    if (outside === "top") {
+      side.y2 -= 0.5;
+      if (p3.y < p2.y) {
+        side.x2 -= 0.5;
+        outside = "left";
+      } else {
+        side.x2 += 0.5;
+        outside = "right"
       }
-      if (!isCorrectlyEnclosed) continue;
-      
-      console.log(area.toFixed(0).padStart(3, " "), "Qualified!   ", `${data[i]!.x},${data[i]!.y}`, "to", `${data[j]!.x},${data[j]!.y}`);
-
-      answer = Math.max(answer, area);
     }
-  } 
+    else if (outside === "right") {
+      side.x2 += 0.5;
+      if (p3.x < p2.x) {
+        side.y2 += 0.5
+        outside = "bottom";
+      } else {
+        side.y2 -= 0.5;
+        outside = "top";
+      }
+    }
+    else if (outside === "bottom") {
+      side.y2 += 0.5;
+      if (p3.y < p2.y) {
+        side.x2 -= 0.5;
+        outside = "left";
+      } else {
+        side.x2 += 0.5;
+        outside = "right";
+      }
+    }
+    else if (outside === "left") {
+      side.x2 -= 0.5;
+      if (p3.x > p2.x) {
+        side.y2 -= 0.5;
+        outside = "top";
+      } else {
+        side.y2 += 0.5;
+        outside = "bottom";
+      }
+    }
+
+    sides.push(side);
+    previousSide = side;
+  }
+
+  const realSides = data.map((p1, i) => ({ x1: p1!.x, y1: p1!.y, x2: data[(i + 1) % data.length]!.x, y2: data[(i + 1) % data.length]!.y }))
+  console.log(`<svg width="600" height="600" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">`);
+  console.log(`<g stroke="red" stroke-width="0.25" stroke-linecap="straight">`);
+  console.log(sides.map(s => `<line x1="${s.x1}" y1="${s.y1}" x2="${s.x2}" y2="${s.y2}" />`).join("\n"))
+  console.log("</g>");
+  console.log(`<g stroke="black" stroke-width="0.25" stroke-linecap="straight">`);
+  console.log(realSides.map(s => `<line x1="${s.x1}" y1="${s.y1}" x2="${s.x2}" y2="${s.y2}" />`).join("\n"))
+  console.log("</g>");
+  console.log("</svg>");
 
   return answer;
 }
